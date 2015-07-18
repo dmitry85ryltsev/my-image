0598ada74cbd47b6db4847176956160e061f1ee7
dmitry85ryltsev <ryltsev.dmitry@gmail.com>
2015-07-18 13:05:04 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    jQuery('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        navigation: false,
+        start: 3,
+        play: {
+            auto: true
+        },
+		navigation: {
+			active: true
+		}
+    });
+
+})(jQuery)
